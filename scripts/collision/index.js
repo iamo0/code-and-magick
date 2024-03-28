d220b9f370b9c1de47275d3710a80517f008ed60
Igor Alexeenko <164557400+iamo0@users.noreply.github.com>
2024-03-28 04:50:00 +0000

    Minor stylistic changes

--- scripts/collision/index.js
+++ scripts/collision/index.js
@@ -37,10 +37,10 @@ const Direction = {
 };
 
 const createGameObject = (
-  x,
-  y,
-  width,
-  height,
+  x = 0,
+  y = 0,
+  width = 0,
+  height = 0,
   direction = Direction.RIGHT | Direction.DOWN,
   xSpeed = 0,
   ySpeed = 0,
@@ -64,14 +64,8 @@ const createGameObject = (
 const getObjectsIntersectionRect = (obj1, obj2) => ({
   x: Math.max(obj1.x, obj2.x),
   y: Math.max(obj1.y, obj2.y),
-  width: (
-    Math.min(obj1.x + obj1.width, obj2.x + obj2.width) - 
-    Math.max(obj2.x, obj1.x)
-  ),
-  height: (
-    Math.min(obj1.y + obj1.height, obj2.y + obj2.height) - 
-    Math.max(obj2.y, obj1.y)
-  ),
+  width: Math.min(obj1.x + obj1.width, obj2.x + obj2.width) - Math.max(obj2.x, obj1.x),
+  height: Math.min(obj1.y + obj1.height, obj2.y + obj2.height) - Math.max(obj2.y, obj1.y),
 });
 
 
