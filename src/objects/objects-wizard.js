f9047478dc90ca85a5543e2ac81174e7e05de2d1
Igor Alexeenko <164557400+iamo0@users.noreply.github.com>
2024-03-31 00:13:50 +0000

    Add constructor function for wizard objects. Remove non-zero speed as a default

--- src/objects/objects-wizard.js
+++ src/objects/objects-wizard.js
@@ -1,26 +1,41 @@
 import { 
+  BaseGameObject,
   GameObjectType,
   Direction,
 } from "./objects";
-
-const WIZARD_PARAMETERS = Object.seal({
-  width: 61,
-  height: 84,
-  ySpeed: 10,
-  xSpeed: 2,
-  type: GameObjectType.WIZARD,
-});
 
 const WizardSprite = {
   REGULAR: 'img/wizard.gif',
   REVERSED: 'img/wizard-reversed.gif',
 };
 
+const WIZARD_PARAMETERS = Object.seal({
+  width: 61,
+  height: 84,
+  type: GameObjectType.WIZARD,
+});
+
+
 const getWizardSprite = (wizard) => Boolean(wizard.direction & Direction.LEFT)
   ? WizardSprite.REVERSED
   : WizardSprite.REGULAR;
 
+
+const createWizard = (extendedParameters) => {
+  const nakedWizard = {
+    ...BaseGameObject,
+    ...WIZARD_PARAMETERS,
+    ...extendedParameters,
+  };
+
+  nakedWizard.sprite = getWizardSprite(nakedWizard.direction);
+
+  return nakedWizard;
+};
+
+
 export {
+  createWizard,
   getWizardSprite,
   WIZARD_PARAMETERS,
   WizardSprite,
--- src/objects/objects-wizard.test.js
+++ src/objects/objects-wizard.test.js
@@ -1,15 +1,33 @@
 import { BaseGameObject, Direction } from "./objects";
 import { 
+  createWizard,
   getWizardSprite,
   WIZARD_PARAMETERS,
 } from "./objects-wizard";
 
-describe("Wizard sprite detection", () => {
-  test("Default sprite for wizard object is one that moves right", () => {
-    const myWizard = {
+describe("Wizard constructor function", () => {
+  test("Default wizard parameters should be composed from default object parameters, basic parameters of wizard and sprite", () => {
+    const myWizard = createWizard();
+
+    expect(myWizard).toEqual({
       ...BaseGameObject,
       ...WIZARD_PARAMETERS,
-    };
+      sprite: 'img/wizard.gif',
+    });
+  });
+
+  test("Extended parameters are applied correctly", () => {
+    const myWizard = createWizard({
+      additionalProp: "Additional value",
+    });
+
+    expect(myWizard.additionalProp).toEqual("Additional value");
+  });
+});
+
+describe("Wizard sprite detection", () => {
+  test("Default sprite for wizard object is one that moves right", () => {
+    const myWizard = createWizard();
 
     const sprite = getWizardSprite(myWizard);
     expect(sprite).toEqual('img/wizard.gif');
@@ -32,6 +50,15 @@ describe("Wizard sprite detection", () => {
       ...WIZARD_PARAMETERS,
       direction: Direction.LEFT,
     };
+
+    const sprite = getWizardSprite(myWizard);
+    expect(sprite).toEqual('img/wizard-reversed.gif');
+  });
+
+  test("If wizard is created with left direction as a default, it's created with correct sprite", () => {
+    const myWizard = createWizard({
+      direction: Direction.LEFT,
+    });
 
     const sprite = getWizardSprite(myWizard);
     expect(sprite).toEqual('img/wizard-reversed.gif');
